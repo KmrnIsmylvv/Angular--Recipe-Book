47fdfa9f8999bfa82c7986a967e5076ebed47e4d
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-03-01 11:27:00 +0400

    feat: Adding a Shopping List Edit

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from "../shared/ingredient.model";
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients=[];
+  ingredients: Ingredient[]=[
+    new Ingredient('Apple', 10),
+    new Ingredient('Orange', 5),
+
+  ];
 
   constructor() { }
 
