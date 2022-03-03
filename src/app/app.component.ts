dbcd4e3757c0704bd06f2214d0aa693d81e8a2a3
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-03-03 12:34:07 +0400

    feat: Adding Navigation with Event Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Shop-List';
+
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string){
+    this.loadedFeature = feature;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,9 @@ import { Component } from "@angular/core";
 })
 
 export class HeaderComponent{
+  @Output() featureSelected = new EventEmitter<string>();
 
+  onSelect(feature: string){
+    this.featureSelected.emit(feature);
+  }
 }
