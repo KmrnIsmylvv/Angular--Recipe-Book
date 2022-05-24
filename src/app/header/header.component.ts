05b2590c012ab2a80b966bfa3f396d4e1e23b639
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-05-24 10:39:03 +0400

    feat: adding navigation between pages

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,9 +6,4 @@ import { Component, EventEmitter, Output } from "@angular/core";
 })
 
 export class HeaderComponent{
-  @Output() featureSelected = new EventEmitter<string>();
-
-  onSelect(feature: string){
-    this.featureSelected.emit(feature);
-  }
 }
